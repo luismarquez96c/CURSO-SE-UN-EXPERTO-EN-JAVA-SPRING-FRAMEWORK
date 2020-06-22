138ab8f902abf9da809ebebc00d1fef17d1e8ded
luis <luis.marquez@proyectosfgk.org>
2020-06-21 20:29:04 -0600

    clases 16, 17, 18 y 19.

---
+++ 16 - Operadores/src/main/java/com/midomino/OperadoresRelacionales.java
@@ -0,0 +1,42 @@
+package com.midomino;
+
+public class OperadoresRelacionales {
+
+	public static void main(String[] args) {
+		/*
+		 * CLASE 19: OPERADORES RELACIONALES
+		 * COMPARAR EL CONTENIDO DE UNA VARIABLE CON OTRA.
+		 * LAS OPERACIONES RELACIONALES SIEMPRE NOS DEVOLVERAN UN VALOR BOOLEANO
+		 */
+		
+		int a = 15;
+		int b = 15;
+		boolean resultado;
+		
+		//== -> COMPARAR SI UNA VARIABLE ES IGUAL QUE OTRA
+//		resultado = a == b;
+//		System.out.println("resultado de la operación relacional: " + resultado);
+		
+		//!= -> COMPARAR SI UNA VARIABLE ES DIFERENTE O DESIGUAL QUE OTRA.
+//		resultado = a != b;
+//		System.out.println("resultado de la operación relacional: " + resultado);	
+		
+		//< -> COMPARAR SI UNA VARAIBLE ES MENOR QUE OTRA
+//		resultado = a < b;
+//		System.out.println("resultado de la operación relacional: " + resultado);	
+		
+		//> -> COMPARAR SI UNA VARIABLE ES MAYOR QUE OTRA
+//		resultado = a > b;
+//		System.out.println("resultado de la operación relacional: " + resultado);	
+		
+		//<= -> COMPARAR SI UNA VARIABLE ES MENOR O IGUAL QUE OTRA
+//		resultado = a <= b;
+//		System.out.println("resultado de la operación relacional: " + resultado);	
+		
+		//>= -> COMPARAR SI UNA VARIABLE ES MAYOR O IGUAL QUE OTRA.
+//		resultado = a >= b;
+//		System.out.println("resultado de la operación relacional: " + resultado);	
+		
+	}
+
+}
---
+++ 16 - Operadores/src/main/java/com/midomino/OperadoresUnarios.java
@@ -0,0 +1,37 @@
+package com.midomino;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+		/*
+		 * CLASE 18: OPERADORES UNARIOS.
+		 * ESTOS NOS PERMITEN REALIZAR OPERACIONES SOBRE UN UNICO OPERANDO
+		 */
+		
+		int a = 10;
+		int b = 2;
+		
+		//incremento: ++ -> b += 1 -> b = b + 1
+//		System.out.println(" a + b: " + ( a + ++b ) );
+//		System.out.println( "b: " + b );
+		
+		//decremento: -- -> b -= 1 -> b = b - 1;
+//		System.out.println(" a + b: " + ( a + b-- ) );
+//		System.out.println( "b: " + b );
+		
+		//asignacion explicita +: +
+//		a = 15;
+//		System.out.println( "a: " + a );
+		
+		//asignacion explicita -: -
+//		b = -20;
+//		System.out.println("b: " + b);
+//		System.out.println( "resultado: " + ( +10 + +20 ) );
+		
+		//operador de negacion: ! -> niega un valor booleano
+//		boolean resultado = false;
+//		System.out.println(" resultado: " + !resultado);
+		
+	}
+
+}
